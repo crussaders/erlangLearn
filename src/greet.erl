b81596e2e007795a74a9ba22419afc8508c342cf
manishpandey <pandeym321@gmail.com>
2026-06-20 14:03:24 +0530

    example for Pattern Matching

---
+++ src/greet.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%%% @author manishkumarpandey
+%%% @copyright (C) 2026, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. Jun 2026 1:49 pm
+%%%-------------------------------------------------------------------
+-module(greet).
+-author("manishkumarpandey").
+
+%% API
+-export([greet/2]).
+
+greet(male, Name) ->
+  io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) ->
+  io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) ->
+  io:format("Hello, ~s!", [Name]).
